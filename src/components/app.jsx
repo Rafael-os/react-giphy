31c9e31be0d6cc9a1633e849b3935f959f77d571
rafael osawa <osawa.rafael@gmail.com>
2020-07-28 01:06:13 +0300

    main gif

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 
 import SearchBar from './search_bar.jsx';
+import Gif from './gif.jsx';
+import GifList from './gif_list.jsx'
+
 
 class App extends Component {
 	render () {
@@ -8,6 +11,9 @@ class App extends Component {
 		  <div>
 				<div className="left-scene">
 					<SearchBar />
+					<div className="selected-gif">
+						<Gif id="JQFZzvTP7XmPBmcSdH" />
+					</div>
 				</div>
 				<div className="right-scene"></div>
 		  </div>
