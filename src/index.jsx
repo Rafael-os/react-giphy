320235ac7ffaf35345811d905c5fe2dbefebddf9
rafael osawa <osawa.rafael@gmail.com>
2020-07-27 22:56:37 +0300

    First application

--- src/index.jsx
+++ src/index.jsx
@@ -3,23 +3,6 @@ import ReactDOM from 'react-dom';
 
 import '../assets/stylesheets/application.scss';
 
-class Hello extends Component {
-	constructor(props) {
-		super(props);
-
-		this.state = {
-			clicked: false
-		};
-	}
-
-	render() {
-		return (
-			<div className={this.state.clicked ? 'clicked' : null}
-		)
-	}
-}
-
-
 const root = document.getElementById('root');
 if (root) {
   ReactDOM.render(<Hello name="World" />, root);
