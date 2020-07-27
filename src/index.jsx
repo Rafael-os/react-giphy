9b8b0cdaa27515cd40698980963e8006b105b908
rafael osawa <osawa.rafael@gmail.com>
2020-07-27 22:40:16 +0300

    change state

--- src/index.jsx
+++ src/index.jsx
@@ -3,14 +3,22 @@ import ReactDOM from 'react-dom';
 
 import '../assets/stylesheets/application.scss';
 
-const Hello = ({ name }) => {
-  return (
-    <div>
-      Hello,
-      {name}
-    </div>
-  );
-};
+class Hello extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			clicked: false
+		};
+	}
+
+	render() {
+		return (
+			<div className={this.state.clicked ? 'clicked' : null}
+		)
+	}
+}
+
 
 const root = document.getElementById('root');
 if (root) {
